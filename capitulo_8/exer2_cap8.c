4a65e142f3f8fe2850682a0468f868c30a34f020
Rodolfo <rodolformartins@outlook.com>
2021-02-27 07:13:25 -0300

    modify exer2_cap8.c

--- capitulo_8/exer2_cap8.c
+++ capitulo_8/exer2_cap8.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 
 /*Crie uma estrutura para representar as coordenadas de um ponto no plano (posições X e Y). 
 Em seguida, declare e leia do teclado um ponto e exiba a distância dele até a origem das coordenadas, isto é, a posição (0,0)*/
@@ -12,23 +14,21 @@ struct coordenadas{
 int main(void){
     struct coordenadas c;
     
-    c.X = 26;
-    c.Y = 32;
-
-    int A, B, distancia1, distancia2;
+    float calc;
 
     printf("\nDigite a posicao do plano no eixo X: ");
-    scanf("%d", &A);
+    scanf("%d", &c.X);
 
     printf("\nDigite a posicao do plano no eixo Y: ");
-    scanf("%d", &B);
+    scanf("%d", &c.Y);
 
    //Exibindo a distância do ponto lido anteriormente até a origem das coordenadas declaradas na estrutura.
-    distancia1 = (A - c.X);
-    distancia2 = (B - c.Y);
+   //(x1-x2)² + (y1-y2)² desse resultado tira raiz quadrada.
 
-    printf("\nDistancia entre a origem: %d - %d", distancia1, distancia2);
+    calc = pow(c.X - 0,2) + pow(c.Y - 0,2);
+        
 
+    printf("\nDistancia até origem: %.2f\n", sqrt(calc));
 
     return(0);
 }
